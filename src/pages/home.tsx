d6323eefa6a1c178681ec4ad360a0eb058ee9b28
Jimmy Shan <jimmyshan@Jimmys-MacBook-Pro.local>
2024-12-23 14:04:22 -0800

    day 001 vengance arc flower

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,9 +3,16 @@ import '../index.css';
 import { useRive } from '@rive-app/react-canvas';
 
 const Home = () => {
-  const { RiveComponent } = useRive({
+  const { RiveComponent: ButtonComponent } = useRive({
     src: "/rive/button.riv",
     stateMachines: "button state",
+    autoplay: true,
+    isTouchScrollEnabled: true,
+  });
+
+  const { RiveComponent: FlowerComponent } = useRive({
+    src: "/rive/flower.riv",
+    stateMachines: "spin",
     autoplay: true,
     isTouchScrollEnabled: true,
   });
@@ -15,9 +22,12 @@ const Home = () => {
       <h1 className="header">Crafting playful interfaces from New York City</h1>
       <div className="demo-grid">
         <div className="demo">
-          <RiveComponent className="w-full h-full rounded-lg" />
+          <ButtonComponent className="w-full h-full rounded-lg" />
         </div>
-        {Array.from({ length: 11 }).map((_, index) => (
+        <div className="demo">
+          <FlowerComponent className="w-full h-full rounded-lg" />
+        </div>
+        {Array.from({ length: 10 }).map((_, index) => (
           <div key={index} className="demo"></div>
         ))}
       </div>
