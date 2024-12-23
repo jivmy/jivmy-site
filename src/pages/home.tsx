d6998966eebf8e9c03f5e2e42a68f54a8fea48ac
Jimmy Shan <jimmyshan@Jimmys-MacBook-Pro.local>
2024-12-23 02:55:28 -0800

    update

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../index.css';
-import ColorPalette from '../components/ColorPalette';
 import { useRive } from '@rive-app/react-canvas';
 
 const Home = () => {
@@ -16,12 +15,9 @@ const Home = () => {
       <h1 className="header">Crafting playful interfaces from New York City</h1>
       <div className="demo-grid">
         <div className="demo">
-          <ColorPalette />
-        </div>
-        <div className="demo">
           <RiveComponent className="w-full h-full rounded-lg" />
         </div>
-        {Array.from({ length: 10 }).map((_, index) => (
+        {Array.from({ length: 11 }).map((_, index) => (
           <div key={index} className="demo"></div>
         ))}
       </div>
