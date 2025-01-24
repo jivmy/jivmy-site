60345e157e35214eed3a19a7439b65b80f53e347
Jimmy Shan <jimmy@jshan.ca>
2025-01-24 13:26:32 -0500

    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,7 +72,7 @@ const notes: NoteData[] = [
     content: [
       "In the dark with the building's usual noises, but this is no usual noise. There's a crow in the boiler room. It's about the size of a raccoon. Not sure what it's doing there, or how it got there, and I don't exactly know how to get it out. So, we're leaving. Packing up and heading down the street.",
       "April isn't far off anymore—and as time passes here I'm learning that... I don't like New York. Or at least I'm not in love with it like I was with California. Sure, the people have been fun, but I just don't like living here, day-to-day. Oops, glad to know. I mean it's not like I could have predicted this outcome, but it seems like I'm headed to Spain for the next few years. I don't speak Spanish—I took French. So my chapter in America comes to a close. At least for now.",
-      "I'm so happy to have spent such quality time here and to have met such unique and soulful people. I'm grateful, and humbled by my friends, and for the opportunity to push myself a little bit here. And I am so, so excited for Barcelona coming. I'm not in love the aesthetic of the rat-race here. Just not my thing. Sorry."
+      "I'm so happy to have spent such quality time here and to have met such unique and soulful people. I'm grateful, and humbled by my friends, and for the opportunity to push myself a little bit here. And I am so, so excited for Barcelona coming. I'm not in   love the aesthetic of the rat-race here. Just not my thing. Sorry bye."
     ]
   },
   {
